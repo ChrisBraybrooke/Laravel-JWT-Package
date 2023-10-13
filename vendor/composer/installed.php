23d3989ff606e7c13b8e62b915277d260d24eb50
Christian Braybrooke <chris@purplemountmedia.com>
2023-10-13 21:46:49 +0100

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'name' => 'chrisbraybrooke/jwt',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f97d1588bacc529a4ef61f0acb4e9281a8a2df0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
     ),
-    'reference' => 'bdc3a2b62e2cc8acd646ae9893e8f45e54043fd4',
-    'name' => 'chrisbraybrooke/jwt',
-  ),
-  'versions' => 
-  array (
-    'chrisbraybrooke/jwt' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'bdc3a2b62e2cc8acd646ae9893e8f45e54043fd4',
+    'versions' => array(
+        'chrisbraybrooke/jwt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f97d1588bacc529a4ef61f0acb4e9281a8a2df0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.4.0',
+            'version' => '5.4.0.0',
+            'reference' => 'd2113d9b2e0e349796e72d2a63cf9319100382d2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
-    'firebase/php-jwt' => 
-    array (
-      'pretty_version' => 'v5.4.0',
-      'version' => '5.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd2113d9b2e0e349796e72d2a63cf9319100382d2',
-    ),
-  ),
 );
